3b3937d2b5bfff375b26151e8bef61a59f20872f
ValGorovyi <valvetigu@gmail.com>
2026-04-01 16:06:13 +0300

    add repository level

--- lib/MVVMarch/mvvmLes.dart
+++ lib/MVVMarch/mvvmLes.dart
@@ -1,38 +1,82 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-class ViewModel extends ChangeNotifier {
-  ViewModel() {
-    loadData();
+class User {
+  final int age;
+  User({required this.age});
+
+  User copyWith({int? age}) {
+    return User(age: age ?? this.age);
   }
-  var _intAge = 0;
-  get intAge => _intAge;
+}
+
+class UserRepository {
+  var _user = User(age: 0);
+  User get user => _user;
 
   Future<void> loadData() async {
     final sp = await SharedPreferences.getInstance();
-    _intAge = sp.getInt('intAge') ?? 0;
-    notifyListeners();
+    final ageU = sp.getInt('intAgeUser') ?? 0;
+    _user = User(age: ageU);
   }
 
-  Future<void> inctementAge() async {
-    _intAge += 1;
-    final sharedPreferences = await SharedPreferences.getInstance();
-    sharedPreferences.setInt('intAge', _intAge);
-    notifyListeners();
+  Future<void> saveData() async {
+    final sp = await SharedPreferences.getInstance();
+    sp.setInt('intAgeUser', _user.age);
   }
 
-  Future<void> decrementAge() async {
-    _intAge = max(_intAge - 1, 0);
-    final sharedPreferences = await SharedPreferences.getInstance();
-    sharedPreferences.setInt('intAge', _intAge);
+  void inctementAge() async {
+    _user = _user.copyWith(age: _user.age + 1);
+  }
+
+  void decrementAge() async {
+    _user = _user.copyWith(age: max(_user.age - 1, 0));
+  }
+}
+
+class ViewModelState {
+  final String ageTitle;
+  ViewModelState({required this.ageTitle});
+}
+
+class ViewModel extends ChangeNotifier {
+  final _userRepo = UserRepository();
+  var _state = ViewModelState(ageTitle: '');
+  ViewModelState get state => _state;
+
+  ViewModel() {
+    loadData();
+  }
+
+  void loadData() async {
+    await _userRepo.loadData();
+    _updateState();
+  }
+
+  Future<void> onIncrementButtonPressed() async {
+    _userRepo.inctementAge();
+    _updateState();
+  }
+
+  Future<void> onDecrementButtonPressed() async {
+    _userRepo.decrementAge();
+    _updateState();
+  }
+
+  void _updateState() {
+    final user = _userRepo._user;
+    _state = ViewModelState(ageTitle: user.age.toString());
     notifyListeners();
   }
 }
 
 class MyLessonAppMvvm extends StatelessWidget {
+  const MyLessonAppMvvm({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -49,26 +93,51 @@ class UpperWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final viewModel = context.watch<ViewModel>();
     return Center(
       child: Column(
         mainAxisSize: MainAxisSize.min,
-        children: [
-          ElevatedButton(
-            onPressed: () {
-              viewModel.inctementAge();
-            },
-            child: Text('++'),
-          ),
-          ElevatedButton(
-            onPressed: () {
-              viewModel.decrementAge();
-            },
-            child: Text('--'),
-          ),
-          Text('${viewModel.intAge}'),
+        children: const [
+          IncrementVievButton(),
+          DecrementVievButton(),
+          TextView(),
         ],
       ),
+    );
+  }
+}
+
+class TextView extends StatelessWidget {
+  const TextView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final title = context.select((ViewModel vm) => vm.state.ageTitle);
+    return Text(title);
+  }
+}
+
+class IncrementVievButton extends StatelessWidget {
+  const IncrementVievButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final model = context.read<ViewModel>();
+    return ElevatedButton(
+      onPressed: model.onIncrementButtonPressed,
+      child: Text('++'),
+    );
+  }
+}
+
+class DecrementVievButton extends StatelessWidget {
+  const DecrementVievButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final model = context.read<ViewModel>();
+    return ElevatedButton(
+      onPressed: model.onDecrementButtonPressed,
+      child: Text('--'),
     );
   }
 }
